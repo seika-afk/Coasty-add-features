1626132623ca23fbf70297a589b1589aec7c34d5
seika-afk <artistnoob6@gmail.com>
2026-08-12 01:39:43 +0530

    Migration(Screenshot) : Migration to windows

--- cdBase/screenshot.ts
+++ cdBase/screenshot.ts
@@ -1,14 +1,15 @@
-import { execSync } from "child_process";
 import fs from "fs";
+import screenshot from "screenshot-desktop";
 
-export const screenshot_ = () => {
+export const screenshot_ = async () => {
   const filename = `screens/screenshot-${Date.now()}.png`;
   try {
-    fs.mkdirSync("screens", { recursive: true });
-    execSync(`grim ${filename}`);
-    return filename;
+    fs.mkdirSync("screens", { recusive: true });
+    await screenshot({ filename })
+    return filename
   } catch (error) {
-    console.error(error);
-    return "";
+    console.log(error)
+    return ""
   }
-};
+
+}
