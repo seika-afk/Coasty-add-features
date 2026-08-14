9d5d807d236560c79c729dcb9b184f92ad16607c
Gagan <84296830+seika-afk@users.noreply.github.com>
2026-08-14 00:31:58 -0700

    Fix typo in mkdirSync options and add format

--- cdBase/screenshot.ts
+++ cdBase/screenshot.ts
@@ -4,8 +4,8 @@ import screenshot from "screenshot-desktop";
 export const screenshot_ = async () => {
   const filename = `screens/screenshot-${Date.now()}.png`;
   try {
-    fs.mkdirSync("screens", { recusive: true });
-    await screenshot({ filename })
+    fs.mkdirSync("screens", { recursive: true });
+    await screenshot({ filename,format:"png"})
     return filename
   } catch (error) {
     console.log(error)
